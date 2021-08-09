fefaab749ec447c2347c9e9e3fa112a4ae6b29f2
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-08-09 20:05:28 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        Hello World
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
